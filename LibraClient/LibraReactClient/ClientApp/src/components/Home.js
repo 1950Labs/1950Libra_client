cd08180903a85d62864c29c2d974946453bfcadb
Diego Bascans <diegobascans@hotmail.com>
2019-11-11 21:34:33 -0300

    Update Home.js

--- LibraClient/LibraReactClient/ClientApp/src/components/Home.js
+++ LibraClient/LibraReactClient/ClientApp/src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
             <h1>Libra Net Core Client</h1>
             <p>Welcome to new Libra Client, developed by <a href='http://1950labs.com/' target="_blank">1950Labs</a></p>
             <p>For further information about Libra or this Libra Client feel free to contact us in <b>hello@1950labs.com</b></p>
-            <p>Last update: <b>30th October 2019</b></p>
+            <p>Last update: <b>11th November 2019</b></p>
             <p>Check our code <a href="https://github.com/1950Labs/2019_POC_Libra" target="_blank">here</a></p>
 
         <p>You will be able to:</p>
